621bf4d2aecc8315c50e6db2eae8570ee158f90f
user.email neetusinha07@gmail.comgit --listgit config --global user.email neetucleargitgit config --global user.email <neetusinha07@gmail.com>
2020-08-06 21:10:56 -0400

    guessing random number

--- rock_paper.py
+++ rock_paper.py
@@ -1,4 +1,4 @@
-user1 = input("Enter your name\n?")
+'''user1 = input("Enter your name\n?")
 user2 = input("Enter your name\n")
 user1_ans = input("%s, Choose rocks, paper or scissor\n"% user1)
 user2_ans = input("%s, Choose rocks, paper or scissor\n"% user2)
@@ -9,5 +9,18 @@ elif user1_ans == 'paper' and user2_ans == 'scissor':
 elif user1_ans == 'scissor' and user2_ans == 'rocks':
     print("user2 won")
 elif user1_ans == 'rocks' and user2_ans == 'paper':
-    print('user1 won')
+    print('user1 won')'''
+import random
 
+user = int(input("guess number between 1 to 9\n"))
+guess = random.randint(1,9)
+if user>guess:
+    print('your guess is higher')
+
+user = int(input("try another number\n"))
+if user<guess:
+    print("your guess is lower, try again")
+user = int(input("try another number\n"))
+if user==guess:
+    print("your guess is right")
+    exit()
