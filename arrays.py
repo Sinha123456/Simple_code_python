a97db4270ca732eecb5cf58599db8112e3ce5cc2
user.email neetusinha07@gmail.comgit --listgit config --global user.email neetucleargitgit config --global user.email <neetusinha07@gmail.com>
2020-12-08 18:00:27 -0500

    6 ways to create arrays.

---
+++ arrays.py
@@ -0,0 +1,27 @@
+from numpy import *
+#create arrays in six ways.
+arr = array([1,2,4,5,6], float)
+print(arr)
+lin =linspace(0, 14, 6)
+print("linspace array:", lin)
+ara = arange(0,16,2)
+print("araange array:", ara)
+log = logspace(1,40,5)
+print(log)
+# print('%.2f' %log[0])
+# print(zeros(5))
+# print(ones(5))
+# arr1 = array([1,3,2,4,5,6])
+# arr2 = arr1 + 5
+# arr3 = arr1 + arr2
+# print(arr3)
+# print(arr2)
+# arr4 = arr1.view()
+# print(arr4)
+# arr5 = arr1
+# print(id(arr1))
+# print(id(arr5))
+# arr6 = arr1.copy()
+# print(arr6)
+# print(id(arr1))
+# print(id(arr6))
